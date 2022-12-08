b74f5310ff3a15c14eb85d491a96f92fd5f1a03f
ChuaSengJia <sengjia54@gmail.com>
2022-12-08 23:59:34 +0800

    Profile name

--- clinicassistant-CreateAppointment.php
+++ clinicassistant-CreateAppointment.php
@@ -34,7 +34,10 @@
                  </ul>
                  <ul class="navbar-nav d-flex mb-2 mb-md-0">
                     <li class="nav-item d-flex">
-                        <a class="nav-link" href="clinicassistant-PersonalProfile.php">Welcome Clinic Assistant Sam</a>
+						<a class="nav-link" href="#">Welcome Clinic Assistant Sam</a>
+                    </li>
+					<li class="nav-item d-flex">
+                        <a class="nav-link active" aria-current="page" href="clinicassistant-PersonalProfile.php">Profile</a>
                     </li>
                     <li class="nav-item d-flex">
                         <a class="nav-link" href="#">Logout</a>
@@ -100,10 +103,6 @@
                                 </label>
                             </div>
                         </div>
-
-                    <div class="col-md-7 pt-3">
-                     <a href="clinicassistant-ATD.php" class="btn btn-Warning btn-block">Create Appointment Treatment Details</a>
-                     </div>
 
                     <div class="row align-items-center mt-3">
                         <div class="col-3">
