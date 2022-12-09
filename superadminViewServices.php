dbaa9fd5e8ba41f6a037b3504d88efb352df290b
future69 <33533603+future69@users.noreply.github.com>
2022-12-09 23:58:18 +0800

    Revert "test"
    This reverts commit eb2e7060d8962945d71636ded1d710d688eb7a55.

--- superadminViewServices.php
+++ superadminViewServices.php
@@ -69,11 +69,7 @@
                         </tr>
                         <tr>
                             <td>Service 2</td>
-<<<<<<< HEAD
                             <td>Active</td>
-=======
-                            <td>Open</td>
->>>>>>> parent of f41d0f7 (Merge branch 'main' of https://github.com/future69/SIM-FYP-22-S4-09)
                         </tr>
                         <tr>
                             <td>Service 3</td>
@@ -81,11 +77,7 @@
                         </tr>
                         <tr>
                             <td>Service 4</td>
-<<<<<<< HEAD
                             <td>Active</td>
-=======
-                            <td>Open</td>
->>>>>>> parent of f41d0f7 (Merge branch 'main' of https://github.com/future69/SIM-FYP-22-S4-09)
                         </tr>
                 </tbody>
             </table>
