4b64a8fd51ff5575889efb21ad0e2d37a5a9398b
future69 <33533603+future69@users.noreply.github.com>
2022-12-10 00:05:58 +0800

    Revert "Revert "test""
    This reverts commit dbaa9fd5e8ba41f6a037b3504d88efb352df290b.

--- superadminClinicAccountApplication.php
+++ superadminClinicAccountApplication.php
@@ -82,7 +82,11 @@
                                     <div class="form-check me-5" >
                                         <input class="form-check-input" type="radio" name="flexRadioDefault1" id="flexRadioDefault1">
                                         <label class="form-check-label" for="flexRadioDefault1">
+<<<<<<< HEAD
                                             Active   
+=======
+                                            Open   
+>>>>>>> parent of f41d0f7 (Merge branch 'main' of https://github.com/future69/SIM-FYP-22-S4-09)
                                         </label>
                                     </div>
                                     <div class="form-check">
@@ -107,7 +111,11 @@
                                     <div class="form-check me-5" >
                                         <input class="form-check-input" type="radio" name="flexRadioDefault2" id="flexRadioDefault2">
                                         <label class="form-check-label" for="flexRadioDefault2">
+<<<<<<< HEAD
                                         Active   
+=======
+                                            Open   
+>>>>>>> parent of f41d0f7 (Merge branch 'main' of https://github.com/future69/SIM-FYP-22-S4-09)
                                         </label>
                                     </div>
                                     <div class="form-check">
@@ -133,7 +141,11 @@
                                     <div class="form-check me-5" >
                                         <input class="form-check-input" type="radio" name="flexRadioDefault3" id="flexRadioDefault3">
                                         <label class="form-check-label" for="flexRadioDefault3">
+<<<<<<< HEAD
                                         Active   
+=======
+                                            Open   
+>>>>>>> parent of f41d0f7 (Merge branch 'main' of https://github.com/future69/SIM-FYP-22-S4-09)
                                         </label>
                                     </div>
                                     <div class="form-check">
