26887dec6d42837ef69d8f266fc5a9c2450b6fdc
darisker <darisannleeker@gmail.com>
2022-12-24 03:14:35 +0800

    daris-ATD1

--- clinicassistant-ATD.php
+++ clinicassistant-ATD.php
@@ -57,11 +57,11 @@
 <body>
 <div class="registrationBoxPatient container">
 	<form method="POST">
-			<div class="row justify-content-center align-items-center">
-				<div class="row col-12 text-center pb-5">
+			<div class="row justify-content-md-center align-items-center">
+				<div class="row col-md-auto text-center pb-5">
 					<div class="display-6">Appointment treatment details for name3</div>
 				</div>
-				<div class="row col-7">
+				<div class="row col-md-auto">
 					<table class="table caption-top table-hover table-secondary table-striped ">
 					<caption>Appointment Details</caption>
 						<thead>
@@ -87,7 +87,9 @@
 								<th scope="col">NRIC</th>
 								<th scope="col">Age</th>
 								<th scope="col">Gender</th>
+								<th scope="col">Allergy</th>
 								<th scope="col">Medical History</th>
+								<th scope="col">Treatment History</th>
 							<tr>
 						</thead>
 						<tbody>
@@ -96,56 +98,125 @@
 								<td> S2234567C </td>
 								<td> 34 </td>
 								<td> Male </td>
-								<td><button type="submit" class="btn btn-primary" name="downloadFile">Download</button></td>
+								<td>
+									Penicillin,
+									Epinphrine	
+								</td>
+								<td>
+									<button type="submit" class="btn btn-secondary" name="viewMedHistory">View Records</button><br><br>
+									<button type="submit" class="btn btn-info" name="updateMedHistory">Update Records</button>
+								</td>
+								<td>
+									<button type="submit" class="btn btn-secondary" name="viewTreatHistory">View Records</button>
+								</td>
 							</tr>
 						</tbody>
 					</table>
-					<form class="row justify-content-center align-items-center">
-					<div class="row col-6 align-items-center py-2">
-						<label for="serviceSL" class="col-3 col-form-label">Service:</label>
-						<div class="col-7">
-						  <select class="form-select" name="serviceSL" id="serviceSL" size="2" multiple>
-						    <option value="plceaholder">Decay Remover</option>
-							<option value="plceaholder">Polishing</option>
-							<option value="plceaholder">Tooth Remover</option>
-						  </select>
-						</div>
+					<script>
+						var treatIndex = 2;
+					</script>
+					<table class="table caption-top table-hover table-secondary table-striped " id="treatTable">
+					<caption>Service Details</caption>
+						<thead>
+							<tr>
+								<th scope="col">No.</th>
+								<th scope="col">Service</th>
+								<th scope="col">Location / Area</th>
+								<th scope="col">Assistant</th>
+								<th scope="col">Materials</th>
+								<th scope="col">Procedure</th>
+								<th scope="col">Notes</th>
+							<tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>1</td>
+								<td>
+								<select class="form-select" name="serviceSL" id="serviceSL" size="3">
+									<option value="plceaholder">Decay Remover</option>
+									<option value="plceaholder">Polishing</option>
+									<option value="plceaholder">Tooth Remover</option>
+								</select>
+								</td>
+								<td>
+									<textarea class='form-control' id="serviceLocTA"></textarea>
+								</td>
+								<td>
+								<select class="form-select" name="assistantSL" id="assistantSL" size="3" multiple>
+									<option value="plceaholder">Jacob Lee</option>
+									<option value="plceaholder">John Adams</option>
+									<option value="plceaholder">Michelle Lee</option>
+								</select>
+								</td>
+								<td>
+								<select class="form-select" name="materialSL" id="materialSL" size="3" multiple>
+									<option value="plceaholder">Resin composites</option>
+									<option value="plceaholder">Amalgam alloys</option>
+									<option value="plceaholder">Glass ionomers</option>
+								</select>
+								</td>
+								<td>
+								<textarea class="form-control" aria-label="With textarea" id="procedureTA" size="3" ></textarea>
+								</td>
+								<td>
+								<textarea class="form-control" aria-label="With textarea" id="notesTA" size="3" ></textarea>
+								</td>
+								<script>
+								/*$("select").mousedown(function(e){
+									e.preventDefault();
+
+									var select = this;
+									var scroll = select.scrollTop;
+									e.target.selected = !e.target.selected;
+
+									setTimeout(function(){select.scrollTop = scroll;}, 0);
+
+									$(select).focus();
+								}).mousemove(function(e){e.preventDefault()});
+								*/
+								</script>
+							</tr>
+						</tbody>
+					</table>
+					<div class="d-grid gap-2 d-md-flex justify-content-md-center pt-5">
+					<button type="button" onclick="addRow()" class="btn btn-dark">+ More Service</button>
 					</div>
-					<div class="row col-6 align-items-center py-2">
-						<label for="assistantSL" class="col-3 col-form-label">Assistant(s):</label>
-						<div class="col-9">
-						  <select class="form-select" name="assistantSL" id="assistantSL" size="2" multiple>
-						    <option value="plceaholder">Jacob Lee</option>
-							<option value="plceaholder">John Adams</option>
-							<option value="plceaholder">Michelle Lee</option>
-						  </select>
-						</div>
-					</div>
-					<div class="row col-6 align-items-center py-2">
-						<label for="serviceSL" class="col-3 col-form-label">Allergies:</label>
-						<div class="col-7">
-						<textarea class="form-control" aria-label="With textarea" id="remarksTA" size="3" ></textarea>
-						</div>
-					</div>
-					<div class="row col-6 align-items-center py-2">
-						<label for="assistantSL" class="col-3 col-form-label">Materials:</label>
-						<div class="col-9">
-						  <select class="form-select" name="assistantSL" id="assistantSL" size="3" multiple>
-						    <option value="plceaholder">Resin composites</option>
-							<option value="plceaholder">Amalgam alloys</option>
-							<option value="plceaholder">Glass ionomers</option>
-						  </select>
-						</div>
-					</div>
-					<div class="row col-12  py-2">
-						<label for="remarksTA" class="col-2 col-form-label">Medical History:</label>
-						<button type="submit" class="btn btn-primary col-3 m-2" name="downloadFile">Pull Records</button>
-						<button type="submit" class="btn btn-primary col-3 m-2" name="downloadFile">Add Records</button>
-						<button type="submit" class="btn btn-primary col-3 m-2" name="downloadFile">Edit Records</button>
-						<div class="col-12">
-							<textarea class="form-control" aria-label="With textarea" id="remarksTA"></textarea>
-						</div>
-					</div>
+					<script>
+						function addRow() {
+							treatIndex++;
+							var table = document.getElementById("treatTable");
+							var row = table.insertRow(treatIndex); 
+							var cell0 = row.insertCell(0);
+							var cell1 = row.insertCell(1);
+							var cell2 = row.insertCell(2);
+							var cell3 = row.insertCell(3);
+							var cell4 = row.insertCell(4);
+							var cell5 = row.insertCell(5);
+							var cell6 = row.insertCell(6);
+							treatIndex--;
+							cell0.innerHTML = treatIndex;
+
+							cell1.innerHTML = "<select class='form-select' name='serviceSL' id='serviceSL' size='3'>" +
+												"<option value='plceaholder'>Decay Remover</option>" +
+												"<option value='plceaholder'>Polishing</option>" +
+												"<option value='plceaholder'>Tooth Remover</option>" +
+											"</select>";
+							cell2.innerHTML = "<td><textarea class='form-control' id='serviceLocTA'></textarea></td>";
+							cell3.innerHTML = "<select class='form-select' name='assistantSL' id='assistantSL' size='3' multiple>" +
+												"<option value='plceaholder'>Jacob Lee</option>" +
+												"<option value='plceaholder'>John Adams</option>" +
+												"<option value='plceaholder'>Michelle Lee</option>" +
+											"</select>";
+							cell4.innerHTML = "<select class='form-select' name='assistantSL' id='assistantSL' size='3' multiple>" +
+												"<option value='plceaholder'>Resin composites</option>" +
+												"<option value='plceaholder'>Amalgam alloys</option>" +
+												"<option value='plceaholder'>Glass ionomers</option>" +
+											"</select>";
+							cell5.innerHTML = "<textarea class='form-control' aria-label='With textarea' id='procedureTA' size='3' ></textarea>";
+							cell6.innerHTML = "<textarea class='form-control' aria-label='With textarea' id='remarksTA' size='3' ></textarea>";
+							treatIndex++;
+						}
+					</script>
 					<div class="d-grid gap-2 d-md-flex justify-content-md-center pt-5">
 						<button class="btn btn-danger" name="back" value="back">Back</button>
 						<button class="btn btn-Primary" name="update" value="update">Update</button>
