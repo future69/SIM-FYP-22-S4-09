a061a28cef2644393b137823ce2ff65147744ea1
future69 <33533603+future69@users.noreply.github.com>
2022-12-30 15:52:19 +0800

    Revert "Update clinic admin create employee"
    This reverts commit cf9349bdda15755cd026283913f7363e62eb6126.

--- clinicAdminCreateEmployee.php
+++ clinicAdminCreateEmployee.php
@@ -1,342 +1,150 @@
 <!DOCTYPE html>
 <html>
-
-<head>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-	<link rel="stylesheet" href="CSS/loginCSS.css" type="text/css" />
-</head>
-<header>
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<div class="container-fluid">
-			<a class="navbar-brand mb-0 h1" href="">
-				<img class="d-inline-block align-top" src="images/SuperDentalLogo.png" width="50" height="40" />
-				DiamondDental™
-			</a>
-			<div class="collapse navbar-collapse" id="navigationBar">
-				<ul class="navbar-nav">
-					<li class="nav-item">
-						<a class="nav-link" href="clinicAdminHomepage.php">Home</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link active" aria-current="page" href="clinicAdminUserAccounts.php">User Accounts</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="clinicAdminAppointments.php">Appointments</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="clinicAdminServices.php">Services</a>
-					</li>
-				</ul>
+	<head>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+		<link rel="stylesheet" href="CSS/loginCSS.css" type="text/css"/>
+	</head>
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+				<div class="container-fluid">
+					<a class="navbar-brand mb-0 h1" href="">
+					<img
+					class="d-inline-block align-top"
+					src="images/SuperDentalLogo.png"
+					width="50" height="40"/>
+					DiamondDental™
+					</a>
+					<div class="collapse navbar-collapse" id="navigationBar">
+						<ul class="navbar-nav">
+							<li class="nav-item">
+								<a class="nav-link" href="clinicAdminHomepage.php">Home</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link active" aria-current="page" href="clinicAdminUserAccounts.php">User Accounts</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="clinicAdminAppointments.php">Appointments</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="clinicAdminServices.php">Services</a>
+							</li>
+						</ul>
+					</div>
+					<div class="me-auto">
+						<ul class="navbar-nav">
+							<li class="nav-item">
+								<span class="navbar-brand text-center">Toa Payoh Dental</span>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="potentialPatientHomepage.php">Logout</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+		</nav>
+		<?php 
+			if (isset($_POST['submit'])) {
+				echo '<script>alert("Account created!")</script>';
+			}
+			else if (isset($_POST['back'])) {
+				header("Location:clinicAdminUserAccounts.php");
+			}
+		?>
+	</header>
+	<body>
+		<div class="registrationBoxPatient container">
+			<div class="row justify-content-center align-items-center">
+				<form method="POST">
+					<div class="row justify-content-center ps-5">
+						<div class="col-5">
+							<h1>Create Employee Account</h1>
+						</div>
+					</div>
+					  <div class="row justify-content-center py-2">
+						<label for="usernameTB" class="col-lg-1 col-form-label">Username:</label>
+						<div class="col-lg-4">
+						  <input class="form-control" id="usernameTB">
+						</div>
+					  </div>
+					  <div class="row justify-content-center py-2">
+						<label for="passwordTB" class="col-lg-1 col-form-label">Password:</label>
+						<div class="col-lg-4">
+						  <input type="password" class="form-control" id="passwordTB">
+						</div>
+					  </div>
+					  <div class="row justify-content-center py-2">
+						<label for="fullnameTB" class="col-lg-1 col-form-label">Full name:</label>
+						<div class="col-lg-4">
+						  <input class="form-control" id="fullnameTB">
+						</div>
+					  </div>
+					  <div class="row justify-content-center align-items-center py-2">
+						<label for="genderSL" class="col-lg-1 col-form-label">Gender:</label>
+						<div class="col-lg-4">
+							<select class="form-select" class="form-select" aria-label="Select gender" name="genderSL">
+								<option value="plceaholder">Male</option>
+								<option value="plceaholder">Female</option>
+								<option value="plceaholder">Others</option>
+							</select>
+						</div>
+					  </div>
+					  <div class="row justify-content-center py-2">
+						<label for="nricTB" class="col-lg-1 col-form-label">NRIC:</label>
+						<div class="col-lg-4">
+						  <input class="form-control" id="nricTB">
+						</div>
+					  </div>
+					  <div class="row justify-content-center py-2">
+						<label for="passwordTB" class="col-lg-1 col-form-label">Email:</label>
+						<div class="col-lg-4">
+						  <input type="email" class="form-control" id="emailTB">
+						</div>
+					  </div>
+					  <div class="row justify-content-center align-items-center py-2">
+						<label for="phoneNumTB" class="col-lg-1 col-form-label">Phone Number:</label>
+						<div class="col-lg-4">
+						  <input class="form-control" id="phoneNumTB">
+						</div>
+					  </div>
+					  <div class="row justify-content-center py-2">
+						<label for="usernameTB" class="col-lg-1 col-form-label">Address:</label>
+						<div class="col-lg-4">
+							<input class="form-control" id="clinicAddress">
+						</div>
+					  </div>
+					  <div class="row justify-content-center py-2">
+						<label for="passwordTB" class="col-lg-1 col-form-label">Postal Code:</label>
+						<div class="col-lg-4">
+						  <input class="form-control" id="postalCodeTB">
+						</div>
+					  </div>
+					  <div class="row justify-content-center align-items-center py-2">
+						<label for="roleSL" class="col-lg-1 col-form-label">Role:</label>
+						<div class="col-lg-4">
+							<select class="form-select" class="form-select" aria-label="Select role" name="roleSL">
+								<option value="plceaholder">Clinic Assistant</option>
+								<option value="plceaholder">Dentist</option>
+							</select>
+						</div>
+					  </div>
+					  <div class="row justify-content-center align-items-center py-2">
+						<label for="formFileQualification" class="col-1 col-form-label">Qualifications:</label>
+						<div class="col-lg-4">
+							<input class="form-control" type="file" id="formFileQualification" multiple>	
+						</div>
+					  </div>
+					  <div class="row justify-content-center align-items-center py-2">
+						<label for="passwordTB" class="col-lg-1 col-form-label">Practitioner Number:</label>
+						<div class="col-lg-4">
+						  <input class="form-control" id="phoneNumberTB">
+						</div>
+					  </div>
+					  <div class="d-grid gap-2 d-md-flex justify-content-md-center py-2">
+						<button class="btn btn-danger" name="back" value="back">Back</button>
+						<button type="submit" class="btn btn-primary" name="submit" value="submit">Submit</button>
+					  </div>
+					</form>
+				</div>
 			</div>
-			<div class="me-auto">
-				<ul class="navbar-nav">
-					<li class="nav-item">
-						<span class="navbar-brand text-center">Toa Payoh Dental</span>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="potentialPatientHomepage.php">Logout</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</nav>
-	<?php
-	//Declare error messages
-	$usernameError = $passwordError = $nricError = $fullNameError = $phoneNumError = $emailError = $addressError = $postalCodeError = $dobError = $pracNumberError = $qualificationError = null;
-	$errorMessage = null;
-
-	if (isset($_POST['createEmployee'])) {
-
-		//Declaring
-		$errors = 0;
-		$DBName = "dentalhealthapplicationdb";
-		$roleName = $_POST['roleSL'];
-		$gender = $_POST['genderSL'];
-		$qualification = $_POST['formFileQualification'];
-		$accStatus = "active";
-
-		//Value is at the input boxes incase of wrong entry, dont have to retype 
-		//Declaring, removing backslashes and whitespaces
-		$username = stripslashes($_POST['usernameTB']);
-		$password = stripslashes($_POST['passwordTB']);
-		$nric = stripslashes($_POST['nricTB']);
-		$fullName = stripslashes($_POST['fullNameTB']);
-		$phoneNum = stripslashes($_POST['phoneNumTB']);
-		$email = stripslashes($_POST['emailTB']);
-		$address = stripslashes($_POST['addressTB']);
-		$postalCode = stripslashes($_POST['postalCodeTB']);
-		$pracNumber = stripslashes($_POST['pracNumberTB']);
-
-		//Remove whitespaces
-		$username = trim($_POST['usernameTB']);
-		$password = trim($_POST['passwordTB']);
-		$nric = trim($_POST['nricTB']);
-		$fullName = trim($_POST['fullNameTB']);
-		$phoneNum = trim($_POST['phoneNumTB']);
-		$email = trim($_POST['emailTB']);
-		$address = trim($_POST['addressTB']);
-		$postalCode = trim($_POST['postalCodeTB']);
-		$pracNumber = trim($_POST['pracNumberTB']);
-
-		//Method to validate entries
-		function correctValidation(): int
-		{
-			//Keep track of total false, the number represents the numbers of inputs failed
-			$totalFalseCount = 0;
-
-			if (empty($GLOBALS['username'])) {
-				$GLOBALS['usernameError'] = "Please enter a value";
-				$totalFalseCount++;
-			}
-
-			if (preg_match('/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/', $GLOBALS['password']) == 0) {
-				$GLOBALS['passwordError'] = "Please enter a value";
-				$totalFalseCount++;
-			}
-
-			if (preg_match('/(?i)^[STFG]\d{7}[A-Z]$/', $GLOBALS['nric']) == 0) {
-				$GLOBALS['nricError'] = "Please enter a value";
-				$totalFalseCount++;
-			}
-
-			if (empty($GLOBALS['fullName'])) {
-				$GLOBALS['fullNameError'] = "Please enter a value";
-				$totalFalseCount++;
-			}
-
-			if (preg_match('/^[6,8,9]{1}[0-9]{7}$/', $GLOBALS['phoneNum']) == 0) {
-				$GLOBALS['phoneNumError'] = "Please enter a valid contact number";
-				$totalFalseCount++;
-			}
-
-			if (filter_var($GLOBALS['email'], FILTER_VALIDATE_EMAIL) == false) {
-				$GLOBALS['emailError'] = "Please enter a valid email";
-				$totalFalseCount++;
-			}
-
-			if (empty($GLOBALS['address'])) {
-				$GLOBALS['addressError'] = "Please enter a value";
-				$totalFalseCount++;
-			}
-
-			if (preg_match('/[0-9]{6}/', $GLOBALS['postalCode']) == 0) {
-				$GLOBALS['postalCodeError'] = "Please enter a valid contact number";
-				$totalFalseCount++;
-			}
-
-			if (empty($GLOBALS['pracNumber'])) {
-				$GLOBALS['pracNumberError'] = "Please enter a value";
-				$totalFalseCount++;
-			}
-
-			if (empty($GLOBALS['formFileQualification'])) {
-				$GLOBALS['qualificationError'] = "Please upload a file";
-				$totalFalseCount++;
-			}
-
-			return $totalFalseCount;
-		}
-
-		//Check for any errors
-		if (correctValidation() > 0) {
-			$errorMessage = "Please complete all fields";
-		} else {
-			try {
-				$conn = mysqli_connect("localhost", "root", "", $DBName);
-
-				//Name of the table 
-				$TableName = "useraccount";
-				$TableName2 = "patientprofile";
-
-				//See if any existing username
-				$SQLstringCheckUsername = "SELECT username FROM $TableName" . " where username='" . $username . "'";
-				$queryResultCheckUsername = mysqli_query($conn, $SQLstringCheckUsername);
-
-				//See if any existing nric
-				$SQLstringCheckNRIC = "SELECT username FROM $TableName" . " where nric='" . $nric . "'";
-				$queryResultCheckNRIC = mysqli_query($conn, $SQLstringCheckNRIC);
-
-				//Encrypt password
-				$encryptedPassword = password_hash($password, PASSWORD_DEFAULT);
-
-				//If there are no results means no login info matches, good thing
-				if (mysqli_num_rows($queryResultCheckUsername) > 0) {
-					$errorMessage = "The username is already in use, please try another";
-				} 
-				else if (mysqli_num_rows($queryResultCheckNRIC) > 0) {
-					$errorMessage = "The nric is already in use, please try another";
-				} 
-				else {
-					//Inserts data into DB
-					$SQLstring = "INSERT INTO $TableName " . " (username, password, nric, fullName, roleName, phoneNum, email, address, postal, gender, accStatus) " . " VALUES( '$username', '$encryptedPassword', '$nric', '$fullName', '$roleName', '$phoneNum', '$email', '$address', '$postalCode', '$gender', '$accStatus' )";
-					$SQLstring2 = "INSERT INTO $TableName2 " . " (nric, dob, medHistory, allergies) " . " VALUES('$nric','$dob','$medicalHistory','$allergies')";
-					mysqli_query($conn, $SQLstring);
-					mysqli_query($conn, $SQLstring2);
-					mysqli_close($conn);
-
-					$errorMessage = "Success!";
-
-					//Reset values after success
-					$username = "";
-					$password = "";
-					$fullName = "";
-					$nric = "";
-					$dob = "";
-					$address = "";
-					$postalCode = "";
-					$phoneNum = "";
-					$email = "";
-					$medicalHistory = "";
-					$allergies = "";
-				}
-			} catch (mysqli_sql_exception $e) {
-				echo "<p>Error: unable to connect/insert record in the database.</p>";
-			}
-		}
-	} else if (isset($_POST['back'])) {
-		header("Location:potentialPatientHomepage.php");
-	} else {
-		$username = "";
-		$password = "";
-		$fullName = "";
-		$nric = "";
-		$dob = "";
-		$address = "";
-		$postalCode = "";
-		$phoneNum = "";
-		$email = "";
-		$medicalHistory = "";
-		$allergies = "";
-	}
-
-	?>
-</header>
-
-<body>
-	<div class="registrationBoxPatient container">
-		<div class="row justify-content-center align-items-center">
-			<form method="POST">
-				<div class="row justify-content-center ps-5">
-					<div class="col-5">
-						<h1>Create Employee Account</h1>
-					</div>
-				</div>
-				<div class="row justify-content-center py-2">
-					<label for="usernameTB" class="col-lg-1 col-form-label">Username:</label>
-					<div class="col-lg-4">
-						<input class="form-control" name="usernameTB" id="usernameTB">
-						<div class="errorMessage">
-							<?php echo $usernameError;?>
-						</div>
-					</div>
-				</div>
-				<div class="row justify-content-center py-2">
-					<label for="passwordTB" class="col-lg-1 col-form-label">Password:</label>
-					<div class="col-lg-4">
-						<input type="password" class="form-control" name="passwordTB" id="passwordTB">
-						<div class="errorMessage">
-							<?php echo $passwordError;?>
-						</div>
-					</div>
-				</div>
-				<div class="row justify-content-center py-2">
-					<label for="fullnameTB" class="col-lg-1 col-form-label">Full name:</label>
-					<div class="col-lg-4">
-						<input class="form-control" name="fullnameTB" id="fullnameTB">
-						<div class="errorMessage">
-							<?php echo $fullNameError;?>
-						</div>
-					</div>
-				</div>
-				<div class="row justify-content-center align-items-center py-2">
-					<label for="genderSL" class="col-lg-1 col-form-label">Gender:</label>
-					<div class="col-lg-4">
-						<select class="form-select" class="form-select" aria-label="Select gender" name="genderSL">
-							<option value="male">Male</option>
-							<option value="female">Female</option>
-							<option value="others">Others</option>
-						</select>
-					</div>
-				</div>
-				<div class="row justify-content-center py-2">
-					<label for="nricTB" class="col-lg-1 col-form-label">NRIC:</label>
-					<div class="col-lg-4">
-						<input class="form-control" name="nricTB" id="nricTB">
-						<div class="errorMessage">
-							<?php echo $nricError;?>
-						</div>
-					</div>
-				</div>
-				<div class="row justify-content-center py-2">
-					<label for="passwordTB" class="col-lg-1 col-form-label">Email:</label>
-					<div class="col-lg-4">
-						<input type="email" class="form-control" name="emailTB" id="emailTB">
-						<div class="errorMessage">
-							<?php echo $emailError;?>
-						</div>
-					</div>
-				</div>
-				<div class="row justify-content-center align-items-center py-2">
-					<label for="phoneNumTB" class="col-lg-1 col-form-label">Phone Number:</label>
-					<div class="col-lg-4">
-						<input class="form-control" name="phoneNumTB" id="phoneNumTB">
-						<div class="errorMessage">
-							<?php echo $phoneNumError;?>
-						</div>
-					</div>
-				</div>
-				<div class="row justify-content-center py-2">
-					<label for="usernameTB" class="col-lg-1 col-form-label">Address:</label>
-					<div class="col-lg-4">
-						<input class="form-control" name="clinicAddress" id="clinicAddress">
-						<div class="errorMessage">
-							<?php echo $addressError;?>
-						</div>
-					</div>
-				</div>
-				<div class="row justify-content-center py-2">
-					<label for="passwordTB" class="col-lg-1 col-form-label">Postal Code:</label>
-					<div class="col-lg-4">
-						<input class="form-control" name="postalCodeTB" id="postalCodeTB">
-						<div class="errorMessage">
-							<?php echo $postalCodeError;?>
-						</div>
-					</div>
-				</div>
-				<div class="row justify-content-center align-items-center py-2">
-					<label for="roleSL" class="col-lg-1 col-form-label">Role:</label>
-					<div class="col-lg-4">
-						<select class="form-select" class="form-select" aria-label="Select role" name="roleSL">
-							<option value="clinicAssistant">Clinic Assistant</option>
-							<option value="dentist">Dentist</option>
-						</select>
-					</div>
-				</div>
-				<div class="row justify-content-center align-items-center py-2">
-					<label for="formFileQualification" class="col-1 col-form-label">Qualifications:</label>
-					<div class="col-lg-4">
-						<input class="form-control" type="file" name="formFileQualification" id="formFileQualification" multiple>
-						<div class="errorMessage">
-							<?php echo $qualificationError;?>
-						</div>
-					</div>
-				</div>
-				<div class="row justify-content-center align-items-center py-2">
-					<label for="passwordTB" class="col-lg-1 col-form-label">Practitioner Number:</label>
-					<div class="col-lg-4">
-						<input class="form-control" name="pracNumberTB" id="pracNumberTB">
-						<div class="errorMessage">
-							<?php echo $pracNumberError;?>
-						</div>
-					</div>
-				</div>
-				<div class="d-grid gap-2 d-md-flex justify-content-md-center py-2">
-					<button class="btn btn-danger" name="back" value="back">Back</button>
-					<button type="submit" class="btn btn-primary" name="createEmployee" value="submit">Submit</button>
-				</div>
-			</form>
-		</div>
-	</div>
-</body>
-
+	</body>
 </html>
