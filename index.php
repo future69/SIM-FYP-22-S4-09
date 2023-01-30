6a6d1c207805a1b42b9b6268a0c3b8eed711a60f
future69 <33533603+future69@users.noreply.github.com>
2023-01-30 17:46:58 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 					<a class="navbar-brand mb-0 h1" href="">
 					<img
 					class="d-inline-block align-top"
-					src="https://diamonddentalclinics.com/images/SuperDentalLogo.png"
+					src="public_html/images/SuperDentalLogo.png"
 					width="50" height="40"/>
 					DiamondDental™
 					</a>
