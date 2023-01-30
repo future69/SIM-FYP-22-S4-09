c5fd9990f64d673300486705d5a62f3c0ecaff63
future69 <33533603+future69@users.noreply.github.com>
2023-01-30 19:22:52 +0800

    Update potentialPatientHomepageAftLogin.php

--- potentialPatientHomepageAftLogin.php
+++ potentialPatientHomepageAftLogin.php
@@ -41,7 +41,7 @@ session_start();
 						<a class="nav-link" href="potentialPatientProfile.php">Profile</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="potentialPatientHomepage.php">Logout</a>
+						<a class="nav-link" href="index.php">Logout</a>
 					</li>
 				</ul>
 			</div>
