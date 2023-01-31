fa1616850d3d06e624f62c5faa8bd43b539c0e82
ChuaSengJia <sengjia54@gmail.com>
2023-01-31 18:36:54 +0800

    ahref name change(Clinic assistant

--- clinicassistant-CreatePatient.php
+++ clinicassistant-CreatePatient.php
@@ -19,7 +19,7 @@
 			<div id="navbarCollapse" class="collapse navbar-collapse">
 				<ul class="navbar-nav me-auto mb-2 mb-md-0">
 					<li class="nav-item">
-						<a class="nav-link" href="clinicassistant-HomePage.php">Home</a>
+						<a class="nav-link" href="ClinicAssistant-HomePage.php">Home</a>
 					</li>
 					<li class="nav-item">
 						<a class="nav-link" href="clinicassistant-AppointmentList.php">Appointment</a>
