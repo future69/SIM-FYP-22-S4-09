fac1164b7f3e0ead731f1616df6b390b344a591f
cjiahui24 <105333680+cjiahui24@users.noreply.github.com>
2023-01-31 14:14:43 +0800

    logout button to point to index.php
    change all the logout button to point to index.php

--- clinicassistant-AppointmentList.php
+++ clinicassistant-AppointmentList.php
@@ -45,7 +45,7 @@ session_start();
 						<a class="nav-link" href="clinicassistant-PersonalProfile.php">Profile</a>
 					</li>
 					<li class="nav-item d-flex">
-						<a class="nav-link" href="potentialPatientHomepage.php">Logout</a>
+						<a class="nav-link" href="index.php">Logout</a>
 					</li>
 				</ul>
 			</div>
