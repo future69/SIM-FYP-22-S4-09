8b83755795e023526a726ab8bdaf37d149be82ef
darisker <darisannleeker@gmail.com>
2023-02-04 17:11:21 +0800

    test count

--- dentistHomepage.php
+++ dentistHomepage.php
@@ -38,7 +38,7 @@
 		$queryResult = mysqli_query($conn, $SQLstring);
 		
 		echo $queryResult;
-		// $count = mysqli_num_rows($queryResult);
+		$count = mysqli_num_rows($queryResult);
 	} catch (mysqli_sql_exception $e) {
 		echo "Error in retrieving or linking tables";
 	}
