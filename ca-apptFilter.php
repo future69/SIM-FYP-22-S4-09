35a3a57cbd61f629d06f6b801858a511eb0d1f0a
darisker <darisannleeker@gmail.com>
2023-02-08 00:31:17 +0800

    fix btns

--- ca-apptFilter.php
+++ ca-apptFilter.php
@@ -79,9 +79,9 @@ if(mysqli_num_rows($result) > 0)
 
         //     $output .= '<button type="submit" class="btn btn-success" name="updateApptTreatmentDetails" onclick="location.href=\'dentistUpdateATD.php?apptID='.$row["apptID"].'\'">Update Appointment Treatment Details</button>
         //         ';
-        // } else {
-        //     $output .= '<button type="submit" class="btn btn-secondary" name="viewApptTreatmentDetails" onclick="location.href=\'dentistViewATD.php?apptID='.$row["apptID"].'\'">View Past Appointment Treatment Details</button>
-        //         ';
+        } else {
+            $output .= '<button type="submit" class="btn btn-secondary" name="viewApptTreatmentDetails" onclick="location.href=\'clinicAdminPastAppointmentTreatmentDetails.php?apptID='.$row["apptID"].'\'">View Past Appointment Treatment Details</button>
+                        ';
         }
         $output .= '
                     </td>
