acfb03373d9f613bd9d479d0ed365b1ea8e9ecfc
ChuaSengJia <sengjia54@gmail.com>
2023-02-13 13:02:51 +0800

    Update clinicAdminServices.php
    clinic admin is able to update services

--- clinicAdminServices.php
+++ clinicAdminServices.php
@@ -10,6 +10,8 @@ $clinicAcraNum = $_SESSION["clinicAdminAcraNum"];
 <head>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 	<link rel="stylesheet" href="CSS/loginCSS.css" type="text/css" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="CSS/loginCSS.css" type="text/css" />
 </head>
 <header>
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -47,17 +49,31 @@ $clinicAcraNum = $_SESSION["clinicAdminAcraNum"];
 		</div>
 	</nav>
 	<?php
-	if (isset($_POST['updateService'])) {
-		echo '<script>alert("Services updated!")</script>';
-	}
-
 	$DBName = "u418115598_dentalapp";
 	$conn = mysqli_connect("localhost", "u418115598_superuser", "HjOSN8hM*", $DBName) or die("Connection Failed");
 	$DBclinic = 'clinic';
 	$DBServiceoffered = 'servicesoffered';
 	$DBService = 'service';
+	$submittedvalue = "";
 	$sqlservice = "SELECT * FROM $DBclinic WHERE acraNum = '$clinicAcraNum'";
+	$sqlallservice = "SELECT * FROM $DBService";
 	$resultservice = mysqli_query($conn, $sqlservice);
+	$resultservice2 = mysqli_query($conn, $sqlallservice);
+	$selectArray = array();
+
+	if (isset($_POST['updateService'])) {
+		$serviceStatusArr = array();
+		$serviceStatusArr = $_POST["serviceStatus"];
+		$string = "";
+		for ($i = 0; $i < count($serviceStatusArr); $i++) {
+			if ($serviceStatusArr[$i] !== "suspended") {
+				$string .= $serviceStatusArr[$i] . " ";
+			}
+		}
+		$sqlupdate = "UPDATE $DBclinic SET servicesSelected = '$string' WHERE clinic.acraNum = '".$clinicAcraNum."'";
+		$insertupdate = mysqli_query($conn, $sqlupdate);
+		echo "Services has been updated";
+	}
 
 	?>
 </header>
@@ -71,45 +87,40 @@ $clinicAcraNum = $_SESSION["clinicAdminAcraNum"];
 				<div class="display-6"></div>
 			</div>
 		</div>
-		<div class="row justify-content-center align-items-center pt-5">
-			<div class="row">
-				<div class="col-6 display-6 pb-3">Services(s)</div>
-				<div class="col-6 text-end display-6 pb-3">
-					<form class="justify-content-end align-items-end" method="POST">
+		<form class="justify-content-end align-items-end" method="POST" action="<?= $_SERVER['PHP_SELF']; ?>">
+			<div class="row justify-content-center align-items-center pt-5">
+				<div class="row">
+					<div class="col-6 display-6 pb-3">Services(s)</div>
+
+					<div class="col text-end display-6 pb-3">
 						<button type="submit" class="btn btn-warning" name="updateService">Update</button>
-					</form>
+					</div>
+					<table class="table table-hover table-secondary table-striped ">
+						<thead>
+							<tr>
+								<th scope="col">Service Name</th>
+								<th scope="col">Status</th>
+							<tr>
+						</thead>
+						<tbody>
+							<?php
+							while ($rows = mysqli_fetch_array($resultservice2)) {
+							?>
+								<tr>
+									<td><?php echo $rows['serviceName']; ?></td>
+									<td>
+										<select class="form-select" name="serviceStatus[]" id="serviceStatus">
+											<option value="<?php echo $rows['serviceName']; ?>">Active</option>
+											<option value="suspended" >Suspended</option>
+									</td>
+								<?php
+							}
+								?>
+						</tbody>
+					</table>
 				</div>
-				<table class="table table-hover table-secondary table-striped ">
-					<thead>
-						<tr>
-							<th>Service Name</th>
-						</tr>
-					</thead>
-					<tbody>
-					<?php
-					$datas = array();
-					if (mysqli_num_rows($resultservice) > 0) {
-						//converts string into array
-						while ($rows = mysqli_fetch_assoc($resultservice)) {
-							$datas[] = $rows;
-							//print_r($datas);
-							foreach ($datas as $data) {
-								$strExplode = explode(" ", $data['servicesSelected']);
-								//print_r($strExplode);
-							}
-							//puts the arrays details into tables
-							foreach ($strExplode as $test) {
-								echo "<tr>".
-								"<td>" . $test . "</td>" .
-								"</tr>";
-							}
-						}
-					}
-					?>
-					</tbody>
-				</table>
 			</div>
-		</div>
+		</form>
 	</div>
 </body>
 
